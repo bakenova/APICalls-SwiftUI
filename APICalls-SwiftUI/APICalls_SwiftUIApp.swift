a18b931534aa9011ec238ebe780fb4150538b9cb
Arailym <arailym.bakenovaa@gmail.com>
2023-01-13 02:26:12 +0600

    Initial Commit

---
+++ APICalls-SwiftUI/APICalls_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICalls_SwiftUIApp.swift
+//  APICalls-SwiftUI
+//
+//  Created by Арайлым Бакенова on 13.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct APICalls_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
